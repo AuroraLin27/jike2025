9059bd541e98eedc4a4219b3ef9e04fcceafa0d3
auroralin <auroralin0207@gmail.com>
2025-12-25 11:13:06 +0800

    fix : axios封装和表单校验

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import './index.scss'
+import { Card, Form, Input, Button } from 'antd'
+import logo from '@/assets/logo.png'
 
-export default function Login() {
+const Login = () => {
+  const onFinish = (value) => {
+    console.log(value)
+  }
   return (
-    <div>Login</div>
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+        <Form onFinish={onFinish} validateTrigger="onBlur">
+          <Form.Item
+            name="mobile"
+            rules={[
+              {
+                required: true,
+                message: '请输入手机号'     
+              },
+              {
+                pattern: /^1[3-9]\d{9}$/,
+                message: '请输入正确的手机号格式'
+              }
+            ]}>
+            <Input size="large" placeholder="请输入手机号" />
+          </Form.Item>
+          <Form.Item
+            name="code"
+            rules={[
+              {
+                required: true,
+                message: '请输入验证码'     
+              },
+            ]}>
+            <Input size="large" placeholder="请输入验证码" />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
   )
 }
+
+export default Login
---
+++ src/utils/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://geek.itheima.net/v1_0',
+  timeout: 5000
+})
+
+// 请求发送之前插入自定义的配置（Cans胡的处理）
+request.interceptors.request.use((config)=> {
+    return config
+  }, (error)=> {
+    return Promise.reject(error)
+})
+
+// 添加响应拦截器
+request.interceptors.response.use((response)=> {
+    // 2xx 范围内的状态码都会触发该函数。
+    // 对响应数据做点什么
+    return response.data
+  }, (error)=> {
+    // 超出 2xx 范围的状态码都会触发该函数。
+    // 对响应错误做点什么
+    return Promise.reject(error)
+})
+
+export {request}
