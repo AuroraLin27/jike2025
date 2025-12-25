13fad062193d337d866f012c26de9e6cbb3cf0b4
auroralin <auroralin0207@gmail.com>
2025-12-25 13:24:25 +0800

    token注入

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,7 +1,14 @@
-import React from 'react'
-
+import React, { use } from 'react'
+import { request } from '@/utils'
+import { useEffect } from 'react'
 export default function Layout() {
+  useEffect(() => {
+    request.get('/user/profile')
+  }, [])
   return (
-    <div>Layout</div>
+    <div>
+      Layout
+
+    </div>
   )
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { getToken } from './token'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -7,6 +8,10 @@ const request = axios.create({
 
 // 请求发送之前插入自定义的配置（Cans胡的处理）
 request.interceptors.request.use((config)=> {
+    const token = getToken()
+    if (token) {
+      config.headers.Authorization = `Bear ${token}`
+    }
     return config
   }, (error)=> {
     return Promise.reject(error)
