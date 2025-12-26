3fdd4cc3f4e68a0b5b6178c9cfec095c8f22d1b6
auroralin <auroralin0207@gmail.com>
2025-12-26 10:45:20 +0800

    二级路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,31 @@
 // 路由配置
 import AuthRoute from "@/components/AuthRoute";
+import Article from "@/pages/Article";
+import Home from "@/pages/Home";
 import Layout from "@/pages/Layout";
 import Login from "@/pages/Login";
+import Publish from "@/pages/Publish";
 
 import {createBrowserRouter} from 'react-router-dom'
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <AuthRoute><Layout/></AuthRoute>
+    element: <AuthRoute><Layout/></AuthRoute>,
+    children: [
+      {
+        index: true,
+        element: <Home/>
+      },
+      {
+        path: 'article',
+        element: <Article/>
+      },
+      {
+        path: 'publish',
+        element: <Publish/>
+      }
+    ]
   },
   {
     path: "/login",
