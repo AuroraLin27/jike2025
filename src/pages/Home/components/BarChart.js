1b4c9eeaa091af28ff767d5e8835463094401fcf
auroralin <auroralin0207@gmail.com>
2025-12-26 12:34:52 +0800

    echart组件封装

---
+++ src/pages/Home/components/BarChart.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react'
+import * as echarts from 'echarts'
+
+export default function BarChart({title}) {
+  const chartRef = useRef(null)
+  useEffect(()=>{
+    // 1. 获取渲染图表的dom节点
+    const chartDom = chartRef.current
+    // 2. 初始化生成图表实例对象
+    const myChart = echarts.init(chartDom);
+    // 3. 图标参数
+    const option = {
+      title: {
+        text: title
+      },
+      xAxis: {
+        type: 'category',
+        data: ['Vue', 'React', 'Angular']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [10, 40, 70],
+          type: 'bar'
+        }
+      ]
+    };
+    // 4. 使用图标参数完成渲染
+    option && myChart.setOption(option);
+  })
+  return (
+    <div ref={chartRef} style={{width: '500px', height: '400px'}}></div>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useRef } from 'react'
-import * as echarts from 'echarts'
+import React from 'react'
+import BarChart from './components/BarChart'
+
 
 export default function Home() {
-  const chartRef = useRef(null)
-  useEffect(()=>{
-    // 1. 获取渲染图表的dom节点
-    const chartDom = chartRef.current
-    // 2. 初始化生成图表实例对象
-    const myChart = echarts.init(chartDom);
-    // 3. 图标参数
-    const option = {
-      xAxis: {
-        type: 'category',
-        data: ['Vue', 'React', 'Angular']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          data: [10, 40, 70],
-          type: 'bar'
-        }
-      ]
-    };
-    // 4. 使用图标参数完成渲染
-    option && myChart.setOption(option);
-  })
+  
   return (
     <div>
-      {/* 容器需要有宽高 */}
-      <div ref={chartRef} style={{width: '500px', height: '400px'}}></div>
+      <BarChart title={'测试1'}/>
+      <BarChart title={'测试2'}/>
     </div>
   )
 }
