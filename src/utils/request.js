4916ea5534478d7adac259559a4f9781a79008f2
auroralin <auroralin0207@gmail.com>
2025-12-26 12:10:38 +0800

    处理token失效

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 import axios from 'axios'
-import { getToken } from './token'
+import { getToken, removeToken } from './token'
+import router from '@/router'
 
 const request = axios.create({
   baseURL: 'http://geek.itheima.net/v1_0',
@@ -19,12 +20,15 @@ request.interceptors.request.use((config)=> {
 
 // 添加响应拦截器
 request.interceptors.response.use((response)=> {
-    // 2xx 范围内的状态码都会触发该函数。
-    // 对响应数据做点什么
     return response.data
   }, (error)=> {
-    // 超出 2xx 范围的状态码都会触发该函数。
-    // 对响应错误做点什么
+    // 监控状态码:error.response.status
+    if (error.response.status === 401) {
+      removeToken()
+      router.navigate('/login')
+      // 强制刷新
+      window.location.reload()
+    }
     return Promise.reject(error)
 })
 
